4bda37bfaabb92382c7176ee92d69ee5229935af
lessless <yevhenii.kurtov@gmail.com>
2020-10-24 21:33:29 +0300

    Move send/receive block into Server.next_number

--- lib/server.ex
+++ lib/server.ex
@@ -1,14 +1,33 @@
 defmodule Primify.Server do
-  @state %{highest_prime: 1, next_number: 2}
+  @initial_state %{highest_prime: nil, next_number: nil}
 
-  def start(n \\ 1, caller_pid) do
-    send(caller_pid, %{@state | highest_prime: n})
+  def start(n) do
+    state = increment_next_number(%{@initial_state | next_number: n})
+
+    loop(state)
   end
 
-  def next_number() do
-    1
+  defp loop(state) do
+    receive do
+      {:next_number, pid} ->
+        state = increment_next_number(state)
+        send(pid, state.next_number)
+        loop(state)
+    end
   end
 
-  def next_number() do
+  defp increment_next_number(state) do
+    %{state | next_number: state.next_number + 1}
+  end
+
+  def next_number(pid) do
+    send(pid, {:next_number, self()})
+
+    receive do
+      number -> number
+    after
+      5 ->
+        raise "Server process is not responding"
+    end
   end
 end
--- test/server_test.exs
+++ test/server_test.exs
@@ -4,40 +4,11 @@ defmodule Primify.ServerTest do
   alias Primify.Server
 
   describe "next_number/1" do
-    test "returns a number to check whether its prime or not" do
-      assert Server.next_number() == 1
-    end
-
-    test "increments numbers" do
-      spawn(Primify.Server, :start, [1, self()])
-
-      result =
-        receive do
-          %{highest_prime: 1, next_number: 2} -> true
-          _ -> false
-        end
-
-      assert result
-    end
-
     test "it holds state" do
-      server_pid = spawn(Primify.Server, :start, [1, self()])
+      server_pid = spawn(Primify.Server, :start, [0])
 
       for n <- 1..10 do
-        send(server_pid, :next_number)
-
-        m = n + 1
-
-        result =
-          receive do
-            ^m -> true
-            _ -> false
-          after
-            5 ->
-              IO.puts(:stderr, "No message in 5 milliseconds")
-          end
-
-        assert result == true
+        assert n + 1 == Server.next_number(server_pid)
       end
     end
   end
