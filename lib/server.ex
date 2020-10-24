0da2ddd81b57f9d2ba4828fead71a64669294c94
Ryan Zidago <ryan.zidago@studitemps.de>
2020-10-24 20:04:39 +0200

    added test to check if process holds state

--- lib/server.ex
+++ lib/server.ex
@@ -1,6 +1,14 @@
 defmodule Primify.Server do
+  @state %{highest_prime: 1, next_number: 2}
+
+  def start(n \\ 1, caller_pid) do
+    send(caller_pid, %{@state | highest_prime: n})
+  end
 
   def next_number() do
     1
+  end
+
+  def next_number() do
   end
 end
--- test/server_test.exs
+++ test/server_test.exs
@@ -9,7 +9,36 @@ defmodule Primify.ServerTest do
     end
 
     test "increments numbers" do
-      raise "not implemented"
+      spawn(Primify.Server, :start, [1, self()])
+
+      result =
+        receive do
+          %{highest_prime: 1, next_number: 2} -> true
+          _ -> false
+        end
+
+      assert result
+    end
+
+    test "it holds state" do
+      server_pid = spawn(Primify.Server, :start, [1, self()])
+
+      for n <- 1..10 do
+        send(server_pid, :next_number)
+
+        m = n + 1
+
+        result =
+          receive do
+            ^m -> true
+            _ -> false
+          after
+            5 ->
+              IO.puts(:stderr, "No message in 5 milliseconds")
+          end
+
+        assert result == true
+      end
     end
   end
 end
